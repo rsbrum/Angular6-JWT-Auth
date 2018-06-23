057d27f01b4e72278a38bc1b6778081b5a6a857e
fennelon <rsbrum@gmail.com>
2018-06-23 01:42:56 -0300

    heroku deploy

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'./dist/index.html'));
 });
-console.log(__dirname+'./dist/index.html');
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
